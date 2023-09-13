edd7271a65eeefef9f212e214d01162f0c4db354
Leonardo-acs <leoacs2002@hotmail.com>
2023-09-12 22:00:57 -0300

    adicionando botão de voltar para o login

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -3,6 +3,7 @@ import RegisterFormUserAccountType from "./RegisterFormUserAccountType/RegisterF
 import RegisterFormUserDetails from "./RegisterFormUserDetails/RegisterFormUserDetails";
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Link } from "react-router-dom";
 export default function Register() {
     const [step, setStep] = useState(1);
     const [userData, setUserData] = useState({
@@ -49,13 +50,13 @@ export default function Register() {
     }
 
     return (
-        <div className="bg-white justify-center flex-col flex w-[420px] h-[500px] shadow-md rounded-md border-[1px] border-solid border-white">
+        <div className="bg-white justify-center flex-col flex w-[420px] h-[540px] shadow-md rounded-md border-[1px] border-solid border-white">
             {render()}
-
             <div className='flex justify-end items-end m-10 gap-2'>
                 {step === 1 ? <></> : <button onClick={prevStep} className='text-[#333] bg-white border-[1px] border-solid border-purple-500 rounded-md w-40 h-10 font-bold text-lg cursor-pointer'><FontAwesomeIcon icon={faArrowLeft} /> Voltar</button>}
                 <button onClick={step === 2 ? () => console.log(userData) : nextStep} className='text-white bg-purple-500 border-[1px] border-solid border-purple-500 rounded-md w-40 h-10 font-bold text-lg cursor-pointer'>{step === 2 ? "Confirmar" : "Continuar"}</button>
             </div>
+            <span className="block text-center gap-1 text-gray-500 mb-5">Já tem uma conta ? <Link className="text-blue-500" to={'/loginform'}> Faça login</Link></span>
         </div>
     )
 
